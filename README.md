69aab90486cac7a01703d924068683a5eb9bd6c8
muzzyay <muzzyay@gmail.com>
2020-07-20 08:47:20 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# serverless_twitter_bot_account
+# Chuck Norris Twitter Bot Account
+
+A serverless twitter bot application that tweets one of Chuck Norris jokes based on random category selection every hour.
+
+Runs on **AWS Lambda** , Triggered by **EventBridge (CloudWatch Events)** each hour.
+
+**Twitter Account:** [@DosageOfJokes](https://twitter.com/DosageOfJokes)
+
+
+
+
+
+
---
+++ handler.js
@@ -0,0 +1,85 @@
+const axios = require('axios');
+const Twitter = require('twit');
+
+
+const config = {
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_SECRET_KEY,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+const client = new Twitter(config);
+
+module.exports.hello = async (event, context, cb) => {
+
+    try{
+
+
+        const category = await selectRandomCategory();
+        const joke = await fetchJoke(category);
+
+        const response = await client.post("statuses/update", {
+            status: joke.trim()+ `\n\n#jokes #funny #ChuckNorris #${category}jokes`,
+            
+        });
+
+        return response;
+
+    }catch(err){
+        return {
+            error: err
+        }
+    }
+    
+}
+
+
+async function fetchJoke(category='food'){
+    try{
+      const joke =  await axios({
+        "method":"GET",
+        "url":`${process.env.JOKE_API_URL}/random`,
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+        "x-rapidapi-key":"778179837amsh3ac9e28fdfd5476p11802ejsneca065d0c5bf",
+        "accept":"application/json",
+        "useQueryString":true
+        },
+        "params":{
+            "category": category
+        }
+        }).then(res=>res.data);
+
+        return joke.value;
+
+    }catch(err){
+        return err;
+    }
+}
+
+async function selectRandomCategory(){
+    try{
+        const categories =  await axios({
+          "method":"GET",
+          "url":`${process.env.JOKE_API_URL}/categories`,
+          "headers":{
+          "content-type":"application/octet-stream",
+          "x-rapidapi-host":"matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+          "x-rapidapi-key":process.env.RAPID_API_KEY,
+          "accept":"application/json",
+          "useQueryString":true
+          }
+          }).then(res=>{
+              const unwantedCategories = ['explicit', 'political', 'religion'];
+
+            return res.data.filter(category=>!unwantedCategories.includes(category));
+          });
+  
+          return categories[parseInt(Math.random()*categories.length)];
+  
+      }catch(err){
+          return err;
+      }
+}
